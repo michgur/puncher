5f166300ed067f873f04f0418ef03d40f0200f11
michgur <michael@thegur.net>
2024-02-17 16:26:24 +0200

    remove expired transactions

--- app/otp/otp.go
+++ app/otp/otp.go
@@ -2,16 +2,35 @@ package otp
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 	"time"
 
 	"encoding/base32"
 
+	"github.com/pquerna/otp"
 	"github.com/pquerna/otp/totp"
 )
 
-var validateOpts = totp.ValidateOpts{
-	Period: 60,
-	Digits: 4,
+var validateOpts totp.ValidateOpts
+
+func init() {
+	// get otp lifetime from environment variable OTP_LIFETIME_SECONDS
+	otpLifetime, err := strconv.Atoi(os.Getenv("OTP_LIFETIME_SECONDS"))
+	if err != nil {
+		fmt.Println("error getting OTP_LIFETIME_SECONDS from environment", err)
+		otpLifetime = 60
+	}
+	digits, err := strconv.Atoi(os.Getenv("OTP_DIGITS"))
+	if err != nil {
+		fmt.Println("error getting OTP_DIGITS from environment", err)
+		digits = 4
+	}
+	// setup the validation options
+	validateOpts = totp.ValidateOpts{
+		Period: uint(otpLifetime),
+		Digits: otp.Digits(digits),
+	}
 }
 
 var b32NoPadding = base32.StdEncoding.WithPadding(base32.NoPadding)
@@ -36,6 +55,13 @@ func GenerateOTP(cardID string, cardSecret string) (otp string, err error) {
 	return
 }
 
+func tryRemoveTransaction(key TransactionKey) {
+	err := RemoveTransaction(key)
+	if err != nil {
+		println(err)
+	}
+}
+
 func ValidateOTP(cardID string, cardSecret string, otp string) bool {
 	// get all transaction keys for the card
 	keys, err := GetTransactions(cardID)
@@ -45,16 +71,14 @@ func ValidateOTP(cardID string, cardSecret string, otp string) bool {
 
 	// validate the OTP using the card's secret and the unique transaction ID
 	for _, key := range keys {
+		if key.UnixTime+int64(validateOpts.Period) < time.Now().Unix() {
+			go tryRemoveTransaction(key)
+		}
 		secret := cardSecret + intToB32(key.TransactionID)
 		valid, err := totp.ValidateCustom(otp, secret, time.Now(), validateOpts)
 		if err == nil && valid {
 			// asynchonously remove the transaction key
-			go func() {
-				err = RemoveTransaction(key)
-				if err != nil {
-					println(err)
-				}
-			}()
+			go tryRemoveTransaction(key)
 			return true
 		}
 	}
